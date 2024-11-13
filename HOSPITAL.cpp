84b33f5246c4a7fea70d25b7c4728c1f5e34dcc5
Rodrigo160701 <rodrigoam@msmk.university>
2024-11-13 20:28:21 +0100

    Agregar archivos de proyecto.

---
+++ HOSPITAL.cpp
@@ -0,0 +1,12 @@
+﻿// HOSPITAL.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "HOSPITAL.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
